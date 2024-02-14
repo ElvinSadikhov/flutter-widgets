202d6a9787aca806952d994862daa03b4fc7588c
elvinsadikhov <elvin.279.6s@gmail.com>
2024-02-14 13:28:54 +0400

    update

--- packages/syncfusion_flutter_datepicker/example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ packages/syncfusion_flutter_datepicker/example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/elvinsadikhov/DevelopmentTools/flutter_3.10.0"
+export "FLUTTER_ROOT=/Users/elvinsadikhov/DevelopmentTools/flutter_3.16.7"
 export "FLUTTER_APPLICATION_PATH=/Users/elvinsadikhov/FlutterDevelopment/Forks/flutter-widgets/packages/syncfusion_flutter_datepicker/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/elvinsadikhov/FlutterDevelopment/Forks/flutter-widgets/packages/syncfusion_flutter_datepicker/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9kNDRiNWE5NGM5NzZmYmI2NTgxNTM3NGY2MWFiNTM5MmEyMjBiMDg0Lw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/elvinsadikhov/FlutterDevelopment/Forks/flutter-widgets/packages/syncfusion_flutter_datepicker/example/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
